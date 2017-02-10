8da3eeeaf1ddf613e1d8195109715b11b4267faa
Bar Eitan <bareitan@microsoft.com>
2017-02-10 11:30:09 +0200

    Before adding form

---
+++ components/Main.jsx
@@ -0,0 +1,19 @@
+var React = require('react');
+
+// var Main = React.createClass({
+//     render: function () {
+//       return (
+//         <h2>Main Component</h2>
+//       );
+//     }
+// });
+
+
+var Main = React.createClass({
+  render: function () {
+    return (
+      <h2>Main component</h2>
+    )
+  }
+})
+module.exports = Main;
