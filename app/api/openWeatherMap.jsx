dff20c5741b13e4421ffc9cf2013695f8dd47931
Bar Eitan <bareitan@microsoft.com>
2017-02-11 12:13:14 +0200

    Styles added

--- app/api/openWeatherMap.jsx
+++ app/api/openWeatherMap.jsx
@@ -3,18 +3,18 @@ var axios = require('axios');
 const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=a28fe22a36f50ceb3981e78e65871875&units=metric';
 
 module.exports = {
-  getTemp: function (location){
+  getTemp: function (location) {
     var encodedLocation = encodeURIComponent(location);
     var requestUrl = `${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`;
 
-    return axios.get(requestUrl).then(function(res) {
-      if(res.data.cod && res.data.message) {
+    return axios.get(requestUrl).then(function (res) {
+      if (res.data.cod && res.data.message) {
         throw new Error(res.data.message);
-      }else {
+      } else {
         return res.data.main.temp;
       }
-    },function (res) {
-      throw new Error(res.data.message);
+    }, function (res) {
+      throw new Error(res.response.data.message);
     });
   }
 }
--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -2,7 +2,7 @@ var React = require('react');
 
 var WeatherMessage = ({temp,location}) =>{
     return (
-      <p className="text-center">It's now {temp} degrees in {location}</p>
+      <h3 className="text-center">It's now {temp} degrees in {location}</h3>
     )
   }
 
